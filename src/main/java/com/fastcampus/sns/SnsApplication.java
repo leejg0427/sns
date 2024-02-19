0de8bbc6f7b388d2031fb8dde307bb3172fba13a
leejg0427 <leejg0427@gmail.com>
2024-02-19 15:17:47 +0900

    init : initialize project

---
+++ src/main/java/com/fastcampus/sns/SnsApplication.java
@@ -0,0 +1,16 @@
+package com.fastcampus.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsApplication.class, args);
+	}
+
+}
